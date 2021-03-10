bc3bb75fa59c82e4997b2a95243b1dafdf0e465f
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-09 23:22:54 -0300

    feat: react props

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,10 @@
-const repositoryName = 'unform';
+import { RepositoryItem } from './RepositoryItem';
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in react',
+  link: 'https://github.com/unform/unform',
+};
 
 export function RepositoryList() {
   return (
@@ -6,26 +12,10 @@ export function RepositoryList() {
       <h1>Repository List</h1>
 
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">Access repository</a>
-        </li>
-
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">Access repository</a>
-        </li>
-
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">Access repository</a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
