134f4dad06d9e76f6518dbe775c51b9720ed91fb
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-10 11:56:53 -0300

    style: li container

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,31 @@
 export function RepositoryItem({ repository }) {
   return (
     <li>
-      <strong>{repository.name ?? 'Default'}</strong>
-      <p>{repository.description}</p>
-
-      <a href={repository.html_url}>Access repository</a>
+      <div className="header">
+        <img src={repository.owner.avatar_url} alt="Owner" />
+        <div>
+          <strong>{repository.name ?? 'Default'}</strong>
+          <p>{repository.description}</p>
+        </div>
+      </div>
+      <a href={repository.html_url}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          class="feather feather-external-link"
+        >
+          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+          <polyline points="15 3 21 3 21 9"></polyline>
+          <line x1="10" y1="14" x2="21" y2="3"></line>
+        </svg>
+      </a>
     </li>
   );
 }
