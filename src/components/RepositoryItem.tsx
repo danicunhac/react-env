8d80ee55f69bac62c40cefaec88d36f1d734a89a
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-10 22:14:36 -0300

    feat: typescript to components

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,4 +1,16 @@
-export function RepositoryItem({ repository }) {
+export interface RepositoryItemProps {
+  repository: {
+    id: string | number;
+    name: string;
+    description: string;
+    html_url: string;
+    owner: {
+      avatar_url: string;
+    };
+  };
+}
+
+export function RepositoryItem({ repository }: RepositoryItemProps) {
   return (
     <li>
       <div className="header">
@@ -19,7 +31,7 @@ export function RepositoryItem({ repository }) {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          class="feather feather-external-link"
+          className="feather feather-external-link"
         >
           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
           <polyline points="15 3 21 3 21 9"></polyline>
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,21 @@
 import { useEffect, useState } from 'react';
 import { RepositoryItem } from './RepositoryItem';
+import { RepositoryItemProps } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
+interface Repository {
+  id: string | number;
+  name: string;
+  description: string;
+  html_url: string;
+  owner: {
+    avatar_url: string;
+  };
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/danicunhac/repos')
@@ -17,8 +28,8 @@ export function RepositoryList() {
       <h1>Repository List</h1>
 
       <ul>
-        {repositories.map((repo) => (
-          <RepositoryItem key={repo.id} repository={repo} />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.id} repository={repository} />
         ))}
       </ul>
     </section>
